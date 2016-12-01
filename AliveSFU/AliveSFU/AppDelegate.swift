8b659cc3204db9c284d30b196f172776f1d01287
Jim <jim@d207-023-170-212.wireless.sfu.ca>
2016-11-30 16:59:51 -0800

    initial Firebase setup

--- AliveSFU/AliveSFU/AppDelegate.swift
+++ AliveSFU/AliveSFU/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         DataHandler.initDataHandlerData()
+        
+        FIRApp.configure() //configure firebase
         
         let flags = DataHandler.getFlags()
         
---
+++ AliveSFU/Pods/Target Support Files/Pods-AliveSFUTests/Pods-AliveSFUTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AliveSFUTests : NSObject
+@end
+@implementation PodsDummy_Pods_AliveSFUTests
+@end
